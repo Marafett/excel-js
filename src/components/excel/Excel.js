1de5e8b1d6d6eeb89aa0483eed3c4862ab640f0b
yuri <marafet1516@gmail.com>
2021-03-28 17:04:57 +0800

    basic table logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,16 +1,19 @@
 import {$} from '@core/dom';
+import {Emmiter} from '@core/Emmiter';
 
 export class Excel {
     constructor(selector, options) {
         this.$el = $(selector)
         this.components = options.components || []
+        this.emmiter = new Emmiter()
     }
 
     getRoot () {
         const $root = $.create('div', 'excel')
+        const componentOptions = { emmiter: this.emmiter }
         this.components = this.components.map(Component => {
             const $el = $.create('div', Component.className)
-            const component = new Component($el)
+            const component = new Component($el, componentOptions)
             // Временно
             if (component.name) {
                 window['c' + component.name] = component
@@ -24,5 +27,8 @@ export class Excel {
     render () {
         this.$el.append(this.getRoot())
         this.components.forEach(component => component.init())
+    }
+    destroy() {
+        this.components.forEach(component => component.destroy())
     }
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,20 +1,41 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
 import {$} from '@core/dom';
+import {TableSelection} from '@/components/table/Table-selection';
+import {range} from '@core/utils';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
-    constructor($root) {
+    constructor($root, options) {
         super($root, {
             name: 'Table',
-            listeners: ['mousedown']
+            listeners: ['mousedown', 'keydown', 'input'],
+            ...options
         });
     }
 
     toHTML() {
         return createTable(20)
     }
+
+    prepare() {
+        this.selection = new TableSelection()
+    }
+
+    init() {
+        super.init()
+
+        const $cell = this.$root.find('[data-id="0:0"]')
+        this.selection.select($cell)
+        this.$on('formula:input', text => {
+            this.selection.current.text(text)
+        })
+        this.$on('formula:done', () => {
+            this.selection.current.focus()
+        })
+    }
+
     onMousedown(event) {
         if (event.target.dataset.resize) {
             const $resizer = $(event.target)
@@ -53,6 +74,57 @@ export class Table extends ExcelComponent {
                     right: 0
                 })
             }
+        } else if (event.target.dataset.type === 'cell') {
+            const $target = $(event.target)
+            if (event.shiftKey) {
+                const target = $target.id(true)
+                const current = this.selection.current.id(true)
+
+                const cols = range(current.col, target.col)
+                const rows = range(current.row, target.row)
+
+                const ids = cols.reduce((acc, col) => {
+                    rows.forEach(row => acc.push(`${row}:${col}`))
+                    return acc
+                }, [])
+                const $cells = ids.map(id => this.$root.find(`[data-id="${id}"]`))
+                this.selection.selectGroup($cells)
+            } else {
+                this.selection.select($target)
+            }
         }
     }
+    onKeydown(event) {
+        const keys = ['Enter', 'Tab', 'ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp']
+        if (keys.includes(event.key) && !event.shiftKey) {
+            event.preventDefault()
+            const id = this.selection.current.id(true)
+            const $next = this.$root.find(nextSelector(event.key, id))
+            this.selection.select($next)
+            this.$emit('table:select', $next)
+        }
+    }
+    onInput(event) {
+        this.$emit('table:input', $(event.target))
+    }
+}
+
+function nextSelector(key, {col, row}) {
+    switch (key) {
+        case 'Enter':
+        case 'ArrowDown':
+            row++
+            break
+        case 'Tab':
+        case 'ArrowRight':
+            col++
+            break
+        case 'ArrowLeft':
+            col = col - 1 < 0 ? 0 : col -1
+            break
+        case 'ArrowUp':
+            row = row - 1 < 0 ? 0 : row -1
+            break
+    }
+    return `[data-id="${row}:${col}"]`
 }
